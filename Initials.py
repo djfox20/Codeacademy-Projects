3d13857f880ce054d6206ab6340d3d64e0184e54
djfox20 <dj_fox20@hotmail.com>
2022-01-01 13:31:32 +0000

    Create Initials.py

---
+++ Initials.py
@@ -0,0 +1,10 @@
+# Adolfo Marcelo
+# Fun Fact:------------------------------------------>Yep, that's me.
+
+print("   A     QQQ  ")
+print("  A A   Q   Q ")
+print(" A   A  Q   Q ")
+print(" AAAAA  Q   Q ")
+print(" A   A  Q   Q ")
+print(" A   A  Q  Q  ")
+print(" A   A   QQ Q ")
